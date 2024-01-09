7b439cae2618c0ba7ef735eac1dd4d58c3ec8d9b
Documenter.jl <documenter@juliadocs.github.io>
2024-01-09 16:22:03 +0000

    build based on b5f20b3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PublicPkgTest","category":"page"},{"location":"#PublicPkgTest","page":"Home","title":"PublicPkgTest","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PublicPkgTest.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PublicPkgTest]","category":"page"}]
+}
